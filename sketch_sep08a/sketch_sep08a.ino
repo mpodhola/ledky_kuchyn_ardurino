1041447bd109a9e2e3ea92ec093d319866d6a381
Martin Podhola <martin.podhola@datavision.cz>
2025-04-02 20:36:49 +0200

    Add programs for testing

---
+++ sketch_sep08a/sketch_sep08a.ino
@@ -0,0 +1,21 @@
+
+#include <IRremote.h>
+
+const int RECV_PIN = 7;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+  irrecv.blink13(true);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (irrecv.decode(&results)){
+        Serial.println(results.value, HEX);
+        irrecv.resume();
+}
+}
